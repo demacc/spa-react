29840fcfaee31c71f89f3939a965b94508be5cd2
Thein Htike Aung <senku9123@gmail.com>
2024-10-05 23:07:27 +0630

    twoWaybindingMethod

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -6,14 +6,26 @@ export default function PostFrom() {
   //   let changes = (e) => {
   //     setpostChg(e.target.value);
   //   };
+
+  let restPost = () => {
+    setpostChg("");
+    console.log("hit");
+  };
   return (
     <form className="form-container">
       <div className="post-form">
         <label htmlFor="">Title</label>
-        <input type="text" onChange={(e) => setpostChg(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setpostChg(e.target.value)}
+          value={postChg}
+        />
       </div>
       <p>{postChg}</p>
       <div className="post-form">
+        <button type="button" onClick={restPost}>
+          rest form
+        </button>
         <button>Post Now</button>
       </div>
     </form>
